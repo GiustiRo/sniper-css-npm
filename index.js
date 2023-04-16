a55b79a97c4f34ba28e0762803ccf9681e75dff9
rgiusti <rgiusti@alabamasolutions.com>
2023-04-16 19:25:34 -0300

    files splited.

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const fs = require('fs');
-const SniperCSS = fs.readFileSync('./dist/sniper-css.min.css', 'utf8');
+const SniperCSS = fs.readFileSync('./scss/index.scss', 'utf8');
+const flex = fs.readFileSync('./scss/flex.scss', 'utf8');
+const text = fs.readFileSync('./scss/text.scss', 'utf8');
+const variables = fs.readFileSync('./scss/_variables.scss', 'utf8');
 
 module.exports = SniperCSS;
+module.exports.SniperCss = SniperCSS;
+module.exports.flex = flex;
+module.exports.text = text;
+module.exports.variables = variables;
